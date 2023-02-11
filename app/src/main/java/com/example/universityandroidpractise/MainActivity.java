c873b7ccf0638ee417a76755b5ce00c36370356e
tesinitsyn <sitiev174@gmail.com>
2023-02-11 12:36:08 +0300

    Added bunch of logs

--- app/src/main/java/com/example/universityandroidpractise/MainActivity.java
+++ app/src/main/java/com/example/universityandroidpractise/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e(TAG, "on create error");
+        Log.w(TAG, "on create warning");
+        Log.i(TAG, "on create info");
+        Log.d(TAG, "on create debug");
+        Log.v(TAG, "on create debug");
     }
 
     @Override
@@ -27,6 +36,11 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         super.onStart();
+        Log.e(TAG, "on start error");
+        Log.w(TAG, "on start warning");
+        Log.i(TAG, "on start info");
+        Log.d(TAG, "on start debug");
+        Log.v(TAG, "on start debug");
     }
     @Override
     protected void onStop() {
@@ -36,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         super.onStop();
+        Log.e(TAG, "on stop error");
+        Log.w(TAG, "on stop warning");
+        Log.i(TAG, "on stop info");
+        Log.d(TAG, "on stop debug");
+        Log.v(TAG, "on stop debug");
     }
     @Override
     protected void onDestroy() {
@@ -44,7 +63,13 @@ public class MainActivity extends AppCompatActivity {
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+        Log.e(TAG, "on create error");
+        Log.w(TAG, "on create warning");
+        Log.i(TAG, "on create info");
+        Log.d(TAG, "on create debug");
+        Log.v(TAG, "on create debug");
         super.onDestroy();
+
     }
     @Override
     protected void onPause() {
@@ -54,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         super.onPause();
+        Log.e(TAG, "on pause error");
+        Log.w(TAG, "on pause warning");
+        Log.i(TAG, "on pause info");
+        Log.d(TAG, "on pause debug");
+        Log.v(TAG, "on pause debug");
     }
     @Override
     protected void onResume() {
@@ -63,5 +93,10 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
         super.onResume();
+        Log.e(TAG, "on resume error");
+        Log.w(TAG, "on resume warning");
+        Log.i(TAG, "on resume info");
+        Log.d(TAG, "on resume debug");
+        Log.v(TAG, "on resume debug");
     }
 }
